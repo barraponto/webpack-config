2b3a818b42342488f6d1861c177f8c31dae8cbb6
Capi Etheriel <barraponto@gmail.com>
2017-02-25 23:52:55 -0300

    adicionando eslint-plugin-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
   env: {browser: true, es6: true},
   extends: ["eslint:recommended", "eslint-config-google"],
-  parserOptions: {sourceType: "module"}, // para poder processar os imports
+  parserOptions: {
+    // para poder processar os imports
+    sourceType: "module",
+    // para poder processar vue templates
+    jsx: true,
+  },
+  plugins: ['vue'],
+  rules: {
+    'vue/jsx-uses-vars': 2,
+  },
 };
